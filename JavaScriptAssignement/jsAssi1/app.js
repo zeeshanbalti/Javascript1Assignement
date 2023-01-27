517f0a2807a8aa39288328957e5e925b8a639169
zeeshanbalti <zeeshanbalti2003@gmail.com>
2023-01-27 20:39:12 +0500

    first Java Assignment

---
+++ JavaScriptAssignement/jsAssi1/app.js
@@ -0,0 +1,28 @@
+//Question1
+
+
+
+
+//Question2
+
+alert("error! please enter a valid password")
+
+//Question3
+
+alert("well come to JS land... \n Happy coding")
+ 
+
+//Question4
+
+var name= "well come to JS land...";
+
+alert("Happy coding...")
+ alert(name)
+
+//Question5
+
+console.log("hello gys")
+
+
+
+ 
